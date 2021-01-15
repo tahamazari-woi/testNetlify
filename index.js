648b96e978be1b5a9321bc05505af7a0caf24f02
tahamazari10 <taha@trebletogether.com>
2021-01-15 15:41:49 +0500

    test

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let test = document.getElementById("test")
 test.href = `app://deepLink/${window.location}`
-console.log(test)
+// console.log(test)
+
+
+$(document).ready(function(){
+    $('#test')[0].click();
+ });
