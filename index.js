906914e247977a1a6ba85a1d3bf8b786cbaf2777
tahamazari10 <taha@trebletogether.com>
2021-01-20 00:56:48 +0500

    https issue

--- index.js
+++ index.js
@@ -11,6 +11,6 @@
 
 // window.location.href = `app://deepLink/${window.location}`;
 var a = document.createElement('a');
-a.setAttribute('href',"./test.html"); //<-- this is the URL
+a.setAttribute('href',`app://deepLink/${window.location}`); //<-- this is the URL
 document.body.appendChild(a);
 a.click();
